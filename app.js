cb550d0aff75e809c5199dc4a4f3b50cedac97c7
Jacob Tompkins <term171013@gmail.com>
2018-05-22 17:18:43 +0800

    adjusted login process to account for auth token

--- app.js
+++ app.js
@@ -4,22 +4,46 @@ App({
     const that = this
     const host = "http://localhost:3000/"
     console.log("Logging in")
-    wx.login({
-      success: res => {
-        console.log(res)
-        wx.request({
-          url: host + 'login',
-          method: 'POST',
-          data: {
-            code: res.code
-          },
-          success: res => {
-            console.log(res)
-            that.globalData.userId = res.data.userId
-          }
-        })
-      }
+    wx.getStorage({
+      key: 'auth_key',
+      success: function(res) {
+        console.log(44444, res)
+        if (res.data) {
+          wx.request({
+            url: host + `login`,
+            method: 'POST',
+            data: {
+              auth_key: res.data
+            },
+            success: function (res) {
+              console.log(22323232323232323, res)
+              that.globalData.userInfo = res.data.userInfo
+            }
+          })
+        } else {
+          wx.login({
+            success: res => {
+              console.log(0, res)
+              wx.request({
+                url: host + 'login',
+                method: 'POST',
+                data: {
+                  code: res.code
+                },
+                success: res => {
+                  console.log(1, res)
+                  wx.setStorage({
+                    key: 'auth_key',
+                    data: res.data.auth_key,
+                  })
+                }
+              })
+            }
+          })
+        }     
+      },
     })
+ 
   },
   globalData: {
     userInfo: null
