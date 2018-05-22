cb280e0914b2118e90984efbd88fec21f806cc9b
Jacob Tompkins <term171013@gmail.com>
2018-05-22 19:16:16 +0800

    fixed adding of user profile info at login

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,15 @@
 //index.js
 //获取应用实例
 const app = getApp()
+const host = "http://localhost:3000/"
 
 Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    authKey: undefined
   },
   //事件处理函数
   bindViewTap: function() {
@@ -24,7 +26,19 @@ Page({
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
-      
     })
-  }
-})
+    const that = this
+    const auth_key = wx.getStorageSync('auth_key')
+    console.log(123123123123, auth_key)
+    wx.request({
+      url: host + "login",
+      method: "put",
+      data: {
+        auth_key: auth_key,
+        userInfo: that.data.userInfo
+      },
+      success(res) {
+        console.log("last", res)
+      }})
+        }
+    })
