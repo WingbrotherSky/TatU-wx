7fc69091e646553b313334173cb1867d05cbd531
Markey Tan <methawee.tan@gmail.com>
2018-05-22 17:57:37 +0800

    first version of browse

---
+++ pages/browse/mock.js
@@ -0,0 +1,261 @@
+// Path: /api/v1/shops
+
+module.exports = {
+  shops: [
+    {
+      "id": 1,
+      "name": "Temple Tattoo",
+      "description": "The coolest, safest tattoo studio in Chengdu. We do a lot of cool stuff I'm not going to go through in mock",
+      "location": "{:lat=>\"39.9054895\", :long=>\"116.3976317\"}",
+      "phone_number": "12345678912",
+      "banner_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQmVwNbaXuXLHUXHBiew8s6W5ur4MSXt7cQx0PAd8wsAKjh434",
+      "logo_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2MZ6dVHf6bCHWgOY-eJ9DHzzjUpb02q9Af-8-3MhN8mjO634-PA",
+      "user_id": 1,
+      "qr_url": "https://upload.wikimedia.org/wikipedia/commons/0/0b/QR_code_Wikimedia_Commons_%28URL%29.png",
+      "artists": [
+        {
+          "name": "Tianyang",
+          "avatar_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6oUP6AlEYMAG0marnLLRVZ21mZ1Ol_xlKbxXc2SCnpuCbDxzO",
+          "shop_id": 1,
+          "years_of_experience": 6,
+          "weibo_url": "https://weibo.com/u/2511311662?refer_flag=1001030103_&is_hot=1#_loginLayer_1526959157437",
+          "art": [
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            }
+          ]
+        },
+        {
+          "name": "Mutou",
+          "avatar_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3SSTAuIsVDpTmBNUJp_xqz7-b3c3Z_AQG4BX3qCmTKxOyFoe7tw",
+          "shop_id": 1,
+          "years_of_experience": 4,
+          "weibo_url": "https://weibo.com/u/2511311662?refer_flag=1001030103_&is_hot=1#_loginLayer_1526959157437",
+          "art": [
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "id": 2,
+      "name": "Temple Tattoo",
+      "description": "The coolest, safest tattoo studio in Chengdu. We do a lot of cool stuff I'm not going to go through in mock",
+      "location": "{:lat=>\"39.9054895\", :long=>\"116.3976317\"}",
+      "phone_number": "12345678912",
+      "banner_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQmVwNbaXuXLHUXHBiew8s6W5ur4MSXt7cQx0PAd8wsAKjh434",
+      "logo_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2MZ6dVHf6bCHWgOY-eJ9DHzzjUpb02q9Af-8-3MhN8mjO634-PA",
+      "user_id": 2,
+      "qr_url": "https://upload.wikimedia.org/wikipedia/commons/0/0b/QR_code_Wikimedia_Commons_%28URL%29.png",
+      "artists": [
+        {
+          "name": "Tianyang",
+          "avatar_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6oUP6AlEYMAG0marnLLRVZ21mZ1Ol_xlKbxXc2SCnpuCbDxzO",
+          "shop_id": 2,
+          "years_of_experience": 6,
+          "weibo_url": "https://weibo.com/u/2511311662?refer_flag=1001030103_&is_hot=1#_loginLayer_1526959157437",
+          "art": [
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            }
+          ]
+        },
+        {
+          "name": "Mutou",
+          "avatar_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3SSTAuIsVDpTmBNUJp_xqz7-b3c3Z_AQG4BX3qCmTKxOyFoe7tw",
+          "shop_id": 2,
+          "years_of_experience": 4,
+          "weibo_url": "https://weibo.com/u/2511311662?refer_flag=1001030103_&is_hot=1#_loginLayer_1526959157437",
+          "art": [
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "id": 3,
+      "name": "Temple Tattoo",
+      "description": "The coolest, safest tattoo studio in Chengdu. We do a lot of cool stuff I'm not going to go through in mock",
+      "location": "{:lat=>\"39.9054895\", :long=>\"116.3976317\"}",
+      "phone_number": "12345678912",
+      "banner_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQmVwNbaXuXLHUXHBiew8s6W5ur4MSXt7cQx0PAd8wsAKjh434",
+      "logo_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2MZ6dVHf6bCHWgOY-eJ9DHzzjUpb02q9Af-8-3MhN8mjO634-PA",
+      "user_id": 3,
+      "qr_url": "https://upload.wikimedia.org/wikipedia/commons/0/0b/QR_code_Wikimedia_Commons_%28URL%29.png",
+      "artists": [
+        {
+          "name": "Tianyang",
+          "avatar_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6oUP6AlEYMAG0marnLLRVZ21mZ1Ol_xlKbxXc2SCnpuCbDxzO",
+          "shop_id": 3,
+          "years_of_experience": 6,
+          "weibo_url": "https://weibo.com/u/2511311662?refer_flag=1001030103_&is_hot=1#_loginLayer_1526959157437",
+          "art": [
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            }
+          ]
+        },
+        {
+          "name": "Mutou",
+          "avatar_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3SSTAuIsVDpTmBNUJp_xqz7-b3c3Z_AQG4BX3qCmTKxOyFoe7tw",
+          "shop_id": 3,
+          "years_of_experience": 4,
+          "weibo_url": "https://weibo.com/u/2511311662?refer_flag=1001030103_&is_hot=1#_loginLayer_1526959157437",
+          "art": [
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            },
+            {
+              "url": "https://s-media-cache-ak0.pinimg.com/originals/47/11/3b/47113b28d99dd034bd2fbd379caa5e63.jpg"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
