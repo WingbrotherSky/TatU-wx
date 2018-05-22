cc5874eca4d3a03ea10e3ae140b48cedbe54734a
Markey Tan <methawee.tan@gmail.com>
2018-05-22 17:57:37 +0800

    first version of browse

--- pages/browse/browse.js
+++ pages/browse/browse.js
@@ -1,19 +1,92 @@
 // pages/browse/browse.js
+
+const app = getApp()
+const mock = require('./mock')
+const path = require('./mock')
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    inputShowed: false,
+    inputVal: "",
+    shops: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad: function () {
+
+
+    this.setData({ shops: mock.shops })
   },
+
+  showUser: function (e) {
+    const data = e.currentTarget.dataset
+    const user = data.user
+    console.log(user)
+    // wx.navigateTo({
+    //   url: `/pages/show/show?=${user.id}`,
+    // })
+    wx.switchTab({
+      url: `/pages/show/show?=${user.id}`,
+    })
+    console.log(user.id)
+  },
+
+  showInput: function () {
+    this.setData({
+      inputShowed: true
+    });
+  },
+  hideInput: function () {
+    this.setData({
+      inputVal: "",
+      inputShowed: false
+    });
+  },
+  clearInput: function () {
+    this.setData({
+      inputVal: ""
+    });
+  },
+  inputTyping: function (e) {
+    this.setData({
+      inputVal: e.detail.value
+    });
+  },
+
+
+  // START TABBAR
+
+  goBrowse: function (e) {
+    wx.reLaunch({
+      url: '/pages/browse/browse',
+    })
+  },
+
+  goFavourite: function (e) {
+    wx.reLaunch({
+      url: '/pages/favourite/favourite',
+    })
+  },
+  goInbox: function (e) {
+    wx.reLaunch({
+      url: '/pages/inbox/inbox',
+    })
+  },
+  goInfo: function (e) {
+    wx.reLaunch({
+      url: '/pages/info/info',
+    })
+  },
+
+  // END TABBAR
+
+  
 
   /**
    * 生命周期函数--监听页面初次渲染完成
