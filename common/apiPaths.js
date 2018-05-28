c3db73d56000ab51b17b91e5e2769245329aacf9
KELW METZ <jw-phoenix_92678@hotmail.com>
2018-05-28 12:49:52 +0800

    test favorites page with real data

--- common/apiPaths.js
+++ common/apiPaths.js
@@ -1,3 +1,4 @@
+// const host = "https://tatu.wogengapp.cn/api/v1/"
 const host = "http://localhost:3000/api/v1/"
 
 module.exports = {
@@ -8,4 +9,5 @@ module.exports = {
   getArt: host + 'arts/',
   postMessage: host + 'messages',
   newFavorite: host + 'favorites'
+  getAllFavorites: host + "favorites"
 }
--- pages/favourite/favourite.js
+++ pages/favourite/favourite.js
@@ -19,10 +19,17 @@ Page({
    */
   onLoad: function () {
     const that = this
+    const auth_key = wx.getStorageSync("auth_key")
     wx.request({
-      url: paths.getAllShops,
+      // url: paths.getAllShops,
+      url: paths.getAllFavorites,
+      data: {
+        auth_key: auth_key
+      },
       success:(res) => {
-          that.setData({shops: res.data.shops})
+        console.log(res)
+          // that.setData({shops: res.data.shops})
+          that.setData({favorites: res.data.favorites})
       }
     })
   },
@@ -58,6 +65,43 @@ Page({
 
     wx.navigateTo({
       url: `/pages/showimage/showimage?id=${artId}`,
+    })
+  },
+
+  previewImage: function (e) {
+    console.log(e)
+    wx.previewImage({
+      // current: '',
+      urls: [e.currentTarget.dataset.artUrl],
+    //   success: function(res) {},
+    //   fail: function(res) {},
+    //   complete: function(res) {},
+    })
+  },
+
+  Delete: function () {
+    wx.showModal({
+      title: '提示',
+      content: 'Delete?!',
+      success: function (res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+          wx.getSavedFileList({
+            success: function (res) {
+              if (res.fileList.length > 0) {
+                wx.removeSavedFile({
+                  filePath: res.fileList[0].filePath,
+                  complete: function (res) {
+                    console.log(res)
+                  }
+                })
+              }
+            }
+          })
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
     })
   },
 
