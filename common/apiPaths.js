f44f2e60fef1bd56f4002d2dc6096959b366db8e
KELW METZ <jw-phoenix_92678@hotmail.com>
2018-05-28 12:49:52 +0800

    test favorites page with real data

--- common/apiPaths.js
+++ common/apiPaths.js
@@ -1,3 +1,4 @@
+// const host = "https://tatu.wogengapp.cn/api/v1/"
 const host = "http://localhost:3000/api/v1/"
 
 module.exports = {
@@ -6,5 +7,6 @@ module.exports = {
   getAllArtists: host + "artists",
   getArtist: host + "artists/",
   getArt: host + 'arts/',
-  postMessage: host + 'messages'
+  postMessage: host + 'messages',
+  getAllFavorites: host + "favorites"
 }
