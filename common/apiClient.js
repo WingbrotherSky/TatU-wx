c6e0b0c1de40e07e247ffe51385ed66654bee6e9
Jacob Tompkins <term171013@gmail.com>
2018-05-30 20:25:46 +0800

    added api client and refactored requests

---
+++ common/apiClient.js
@@ -0,0 +1,52 @@
+const app = getApp() 
+const host = "https://localhost:3000/api/v1/"
+
+const request = function(method, options) {
+  let auth_key = wx.getStorageSync("auth_key")
+
+  wx.request({
+    url: options.url,
+    method,
+    data: {
+      ...options.data,
+      auth_key: auth_key
+      },
+    success: res => {
+        if (!auth_key) {
+          app.login( () => {
+            request(method, options)
+          })
+        } else {
+          options.success(res)
+        }
+    },
+    fail: res => {
+      console.log("failed", res)
+    }
+  })
+}
+
+const get = (options) => {
+  request('get', options)
+}
+
+const post = (options) => {
+  request('post', options)
+}
+
+const put = (options) => {
+  request('put', options)
+}
+
+const DELETE = (options) => {
+  request('delete', options)
+}
+
+
+
+module.exports = {
+get,
+put,
+post,
+delete: DELETE
+}
